8665ccf6c8f511c2ea3c352506e177f7eb1a9d39
vari <vari@gmail.com>
2024-03-03 00:28:54 +0530

    Added cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
-// app.use(cors());
+app.use(cors());
 
 app.get('/news', async (req, res) => {
   try {
